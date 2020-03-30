6db64386042659dcfa60b55abd3bcb72ba8cb0c5
GitWillG <36644824+GitWillG@users.noreply.github.com>
2020-03-30 15:14:20 -0400

    colorhiding
    title

---
+++ RaheelProj_Android/Assets/Scripts/colorControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorControl : MonoBehaviour
+{
+    public GameObject colorManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        colorManager = GameObject.FindGameObjectWithTag("colorManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(colorManager.GetComponent<colorManager>().curCol);
+        if (colorManager.GetComponent<colorManager>().curCol == "All")
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        }
+
+        else if (colorManager.GetComponent<colorManager>().curCol == gameObject.tag.ToString())
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+
+        }
+        
+    }
+}
---
+++ RaheelProj_Android/Assets/Scripts/colorManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorManager : MonoBehaviour
+{
+    public List<string> color;
+    public string curCol;
+    // Start is called before the first frame update
+    void Start()
+    {
+        color.Add("All");
+        color.Add("RedBlock");
+        color.Add("BlueBlock");
+        color.Add("GreenBlock");
+        curCol = color[0];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void All()
+    {
+
+        curCol = color[0];
+    }
+    public void Red()
+    {
+
+        curCol = color[1];
+    }
+    public void Blue()
+    {
+
+        curCol = color[2];
+    }
+    public void Green()
+    {
+
+        curCol = color[3];
+    }
+}
